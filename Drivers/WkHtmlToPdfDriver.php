80fd9a72cbc628b3f9f8947ca8deaa1afa074dee
Adam Quaile <adamquaile@gmail.com>
2014-04-25 16:36:57 +0100

    Added tests for wk driver

--- Drivers/WkHtmlToPdfDriver.php
+++ Drivers/WkHtmlToPdfDriver.php
@@ -4,11 +4,25 @@ namespace EP\Bundle\HtmlToPdfBundle\Drivers;
 
 class WkHtmlToPdfDriver implements DriverInterface
 {
+    /**
+     * @var \WkHtmlToPdf
+     */
+    private $converter;
 
-    public function __construct()
+    public function __construct(\WkHtmlToPdf $converter = null)
     {
-
+        $this->converter = $converter;
     }
+
+    /**
+     * @param \WkHtmlToPdf $converter
+     */
+    public function setConverter($converter)
+    {
+        $this->converter = $converter;
+    }
+
+
 
     /**
      * Generate a PDF from HTML, and place the resultant file
--- Tests/Drivers/WkHtmlToPdfDriverTest.php
+++ Tests/Drivers/WkHtmlToPdfDriverTest.php
@@ -2,14 +2,47 @@
 
 namespace EP\Bundle\HtmlToPdfBundle\Drivers;
 
+
+use org\bovigo\vfs\vfsStream;
+
 class WkHtmlToPdfDriverTest extends DriverTest
 {
     /**
-     * @return DriverInterface
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $converter;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->converter = $this->getMock('\WkHtmlToPdf');
+    }
+
+
+    /**
+     * @return WkHtmlToPdfDriver
      */
     protected function getDriver()
     {
         return new WkHtmlToPdfDriver();
+    }
+
+    public function testGenerationProcess()
+    {
+        vfsStream::setup();
+
+        $driver = $this->getDriver();
+        $driver->setConverter($this->converter);
+
+        $html = '<html></html>';
+        $outfile = vfsStream::url('root/outfile.pdf');
+
+        $this->converter->expects($this->once())->method('addPage')->with($html);
+        $this->converter->expects($this->once())->method('saveAs')->with($outfile);
+
+        $driver->generate($html, $outfile);
+
     }
 
 }
